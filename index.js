229b38459a8156be552fc2862a28cb025d79923d
huziibee <mhbangie@gmail.com>
2024-04-08 15:06:53 +0200

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //create cars api using express
 const express = require('express');
 const app = express();
-
-
+const path = require('path');
+// const cors = require('cors');
 
 app.use(express.json());
-
+// app.use(cors());
 const cars = require('./cars.json');
 
 //get all cars
@@ -45,6 +45,12 @@ app.post('/cars', (req, res) => {
     cars.push(newCar);
     res.json(newCar);
 });
+
+app.use(express.static(path.resolve(__dirname, './'), { maxAge : '1y', etag: false}));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+  });
 
 //start app at localhost:3001
 app.listen(3001, () => {
